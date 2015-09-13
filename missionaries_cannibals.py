153f7d4131f9bb62c3d9bf624fb26b9849d7234d
Kim Ngo <kngo@nd.edu>
2015-09-13 18:47:36 -0400

    implemented bfs, need to check for correctness

--- missionaries_cannibals.py
+++ missionaries_cannibals.py
@@ -10,6 +10,7 @@
 import operator
 import Queue
 
+WRONGSIDE = 1
 NUM_MISS, NUM_CANN, NUM_BOAT = range(3)
 ACTIONS = [[1,0,1],[2,0,1],[0,1,1],[0,2,1],[1,1,1]]
 GOAL = [0,0,0]
@@ -30,17 +31,19 @@ class node:
 	def create_children(self):
 		# Creates child nodes with 5 posssible actions
 		for action in self.actions:
-			child_value = map(operator.sub, self.value, action)
-			self.children.append(node(child_value))
-		self.valid_children() # ensures only valid children remain
+			if self.value[NUM_BOAT] == WRONGSIDE:
+				child_value = map(operator.sub, self.value, action)
+				self.children.append(node(child_value))
+			else:
+				child_value = map(operator.add, self.value, action)
+				self.children.append(node(child_value))
+		self.valid_children(self.children) # ensures only valid children remain
 
-	def valid_children(self):
+	def valid(self, value):
 		# num_cannibals > num_missionaries are invalid states and are removed from consideration
-		valid_children = []
-		for child in self.get_children():
-			if child.get_value()[NUM_CANN] <= child.get_value()[NUM_MISS] and all(i >= 0 for i in child.get_value()):
-				valid_children.append(child)
-		self.children = valid_children
+		if value[NUM_CANN] <= value[NUM_MISS] and all(i >= 0 for i in value) and value[NUM_CANN] <= 3 and value[NUM_MISS] <= 3:
+			return True
+		return False
 
 def all(iterable):
 	for element in iterable:
@@ -55,23 +58,28 @@ def breadth_first_search(current_state):
 	while not q.empty():
 		state = q.get()
 		explored.add(state)
-		state.create_children()
-		print state.get_value(), len(state.get_children())
-		for child in state.get_children():
-			if child not in explored and child != state:
-				if child == GOAL:
+		for action in ACTIONS:
+			child = None
+			if state.value[NUM_BOAT] == WRONGSIDE:
+				child = node(map(operator.sub, state.value, action))
+			if state.value[NUM_BOAT] != WRONGSIDE:
+				child = node(map(operator.add, state.value, action))
+			if state.valid(child.get_value()) and child not in explored and child != state:
+				if child.get_value() == GOAL:
 					print "GOALLL"
 					return child
 				q.put(child)
-				print "QUEUE SIZE: ", q.qsize()
+				print_line(child.get_value(), action, q.qsize())
 
 #	return current_state, action, nodes_expanded
 
+def print_line(value, action, nodes_expanded):
+	print '<' + ','.join(map(str,value)) + '>, <' + ','.join(map(str,action)) + '>, ' + str(nodes_expanded)
 
 if __name__ == '__main__':
 	initial_state = [3,3,1]
 	current_state = initial_state
 
-	print "breadth-first search"
+	print "Algorithm: breadth-first search"
 	init = node(initial_state)
 	breadth_first_search(init)
